b93fb3b7394000379846e5f32c80b2e7b68de7ab
madhusudhannr40-cloud <madhusudhannr40@gmail.com>
2026-09-12 12:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Smart Student Punctuality Systemstarted!");
+function login() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        document.getElementById("message").innerText =
+            "Please enter username and password.";
+    } 
+    else {
+        document.getElementById("message").innerText =
+            "Login successful! ✅";
+    }
+}
